d054f551f39c1df67c0756e4ff04c53f497a6a23
quang.phan <ptquang2000@gmail.com>
2023-10-12 06:51:55 +0700

    update prob 88 & add readme

--- src/array_string/array_string.c
+++ src/array_string/array_string.c
@@ -5,33 +5,17 @@
 
 static void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
 {
-	if (n == 0) {
-		return;
-	}
-	else if (m == 0) {
-		for (int k = 0; k < nums1Size; k++) {
-			nums1[k] = nums2[k];
+	while (m > 0 || n > 0) {
+		if (n == 0) {
+			break;
 		}
-		return;
-	}
-
-	for (int i = 0, j = 0; i < m + n; i++) {
-		if (i >= m) {
-			nums1[i] = nums2[j];
-			j++;
+		else if (m == 0 || nums1[m - 1] <= nums2[n - 1]) {
+			n--;
+			nums1[m + n] = nums2[n];
 		}
-		else if (nums1[i] > nums2[j]) {
-			int temp = nums1[i];
-			nums1[i] = nums2[j];
-			nums2[j] = temp;
-			
-			for (int k = j; k < n - 1; k++) {
-				if (nums2[k] <= nums2[k + 1]) break;
-			
-				temp = nums2[k];
-				nums2[k] = nums2[k + 1];
-				nums2[k + 1] = temp;
-			}
+		else if (nums1[m - 1] > nums2[n - 1]) {
+			m--;
+			nums1[m + n] = nums1[m];
 		}
 	}
 }
