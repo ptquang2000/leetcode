f9930f425ac9c448771b91692aa5b66015fe1b0b
quang.phantri <ptquang2000@gmail.com>
2025-02-12 07:22:35 +0700

    implement binary heap

--- src/heap/heap.c
+++ src/heap/heap.c
@@ -1,62 +1,81 @@
 #include "heap.h"
+#include "utils/utils.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static size_t parent_index(size_t i)
-{
-        return (i - 1) / 2;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-static size_t left_index(size_t i)
-{
-        return 2 * i + 1;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-static size_t right_index(size_t i)
-{
-        return 2 * i + 2;
-}
+#define parent_idx(i) (((i) - 1) / 2)
+#define lc_idx(i) (2 * (i) + 1)
+#define rc_idx(i) (2 * (i) + 2)
 
 ////////////////////////////////////////////////////////////////////////////////
 
 static void heapify_up(struct heap *h, size_t i)
 {
-        if (i == 0)
+        size_t p = parent_idx(i);
+        if (i <= 0)
                 return;
+
+        if (h->data[p] > h->data[i]) {
+                h->data[p] ^= h->data[i];
+                h->data[i] ^= h->data[p];
+                h->data[p] ^= h->data[i];
+                heapify_up(h, p);
+        }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 static void heapify_down(struct heap *h, size_t i)
 {
-        if (i >= h->size)
+        size_t l = lc_idx(i);
+        size_t r = rc_idx(i);
+        if (i >= h->size || l >= h->size)
                 return;
+
+        size_t m = h->data[l] > h->data[r] ? (h->data[i] > h->data[r] ? r : i) : (h->data[i] > h->data[l] ? l : i);
+        if (m != i) {
+                h->data[m] ^= h->data[i];
+                h->data[i] ^= h->data[m];
+                h->data[m] ^= h->data[i];
+                heapify_down(h, m);
+        }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void heap_push(struct heap *h, size_t v)
 {
+        h->data = realloc(h->data, ++h->size * sizeof(*h->data));
+        h->data[h->size - 1] = v;
+        heapify_up(h, h->size - 1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 size_t heap_pop(struct heap *h)
 {
-        return 0;
+        UTILS_ASSERT_MSG(h->size > 0, "Heap size is 0");
+        size_t v = h->data[0];
+        if (--h->size == 0) {
+                free(h->data);
+                return v;
+        }
+
+        h->data[0] = h->data[h->size];
+        heapify_down(h, 0);
+        return v;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 size_t heap_top(struct heap *h)
 {
-        return 0;
+        UTILS_ASSERT_MSG(h->size > 0, "Heap size is 0");
+        return h->data[0];
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/heap/test/main.c
+++ src/heap/test/main.c
@@ -17,15 +17,15 @@ int main()
         heap_push(h, 8);
         heap_push(h, 7);
 
-	UTILS_ASSERT_EQUAL(h->size, 8);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 1);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 3);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 4);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 5);
-	UTILS_ASSERT_EQUAL(h->size, 4);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 7);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 8);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 69);
-	UTILS_ASSERT_EQUAL(heap_pop(h), 420);
-	UTILS_ASSERT_EQUAL(h->size, 0);
+        UTILS_ASSERT_EQUAL(h->size, 8);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 1);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 3);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 4);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 5);
+        UTILS_ASSERT_EQUAL(h->size, 4);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 7);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 8);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 69);
+        UTILS_ASSERT_EQUAL(heap_pop(h), 420);
+        UTILS_ASSERT_EQUAL(h->size, 0);
 }
