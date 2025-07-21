f0b98db2c5561bea430a81d1b50b37e86dcf0a2f
Quang Phan <ptquang2000@gmail.com>
2025-07-21 22:28:12 +0700

    update dp tc

--- cmd/dp/decompose_into_dictionary_words_test.go
+++ cmd/dp/decompose_into_dictionary_words_test.go
@@ -1,7 +1,7 @@
 package dp
 
 import (
-	"reflect"
+	"slices"
 	"testing"
 
 	. "leetcode/day"
@@ -11,55 +11,55 @@ func TestDecomposeIntoDictionaryWords(T *testing.T) {
 	var actual, expected []string
 
 	expected = []string{"apple"}
-	if actual = DecomposeIntoDictionaryWords("apple", []string{"apple"}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("apple", []string{"apple"}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
 	expected = []string{"apple", "pie"}
-	if actual = DecomposeIntoDictionaryWords("applepie", []string{"apple", "pie"}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("applepie", []string{"apple", "pie"}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
 	expected = []string{}
-	if actual = DecomposeIntoDictionaryWords("xyz", []string{"a", "b", "c"}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("xyz", []string{"a", "b", "c"}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
 	expected = []string{}
-	if actual = DecomposeIntoDictionaryWords("applepie", []string{"app", "pie"}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("applepie", []string{"app", "pie"}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
-	if actual = DecomposeIntoDictionaryWords("pineapple", []string{"pine", "apple", "pineapple"}); !reflect.DeepEqual(actual, []string{"pine", "apple"}) &&
-		!reflect.DeepEqual(actual, []string{"pineapple"}) {
+	if actual = DecomposeIntoDictionaryWords("pineapple", []string{"pine", "apple", "pineapple"}); !slices.Equal(actual, []string{"pine", "apple"}) &&
+		!slices.Equal(actual, []string{"pineapple"}) {
 		T.Logf("Expected %v got %v", []([]string){{"pine", "apple"}, []string{"pineapple"}}, actual)
 		T.Fail()
 	}
 
 	expected = []string{}
-	if actual = DecomposeIntoDictionaryWords("", []string{"a", "b"}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("", []string{"a", "b"}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
 	expected = []string{}
-	if actual = DecomposeIntoDictionaryWords("apple", []string{}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("apple", []string{}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
 	expected = []string{"apple", "apple"}
-	if actual = DecomposeIntoDictionaryWords("appleapple", []string{"apple"}); !reflect.DeepEqual(actual, expected) {
+	if actual = DecomposeIntoDictionaryWords("appleapple", []string{"apple"}); !slices.Equal(actual, expected) {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
-	if actual = DecomposeIntoDictionaryWords("catsanddog", []string{"cat", "cats", "and", "sand", "dog"}); !reflect.DeepEqual(actual, []string{"cats", "and", "dog"}) &&
-		!reflect.DeepEqual(actual, []string{"cat", "sand", "dog"}) {
+	if actual = DecomposeIntoDictionaryWords("catsanddog", []string{"cat", "cats", "and", "sand", "dog"}); !slices.Equal(actual, []string{"cats", "and", "dog"}) &&
+		!slices.Equal(actual, []string{"cat", "sand", "dog"}) {
 		T.Logf("Expected %v got %v", []([]string){{"cats", "and", "dog"}, []string{"cat", "sand", "dog"}}, actual)
 		T.Fail()
 	}
