a725a58cfb34a443026a1bb222dde7ff94dac182
Quang Phan <ptquang2000@gmail.com>
2025-07-22 07:05:26 +0700

    update dp tc

--- cmd/dp/maximum_revenue_test.go
+++ cmd/dp/maximum_revenue_test.go
@@ -10,19 +10,7 @@ func TestMaximumRevenue(T *testing.T) {
 	var actual, expected int
 
 	expected = 5
-	if actual = MaximumRevenue([]int{5}); actual != expected {
-		T.Logf("Expected %v got %v", expected, actual)
-		T.Fail()
-	}
-
-	expected = 9
-	if actual = MaximumRevenue([]int{3, 9}); actual != expected {
-		T.Logf("Expected %v got %v", expected, actual)
-		T.Fail()
-	}
-
-	expected = 4
-	if actual = MaximumRevenue([]int{1, 2, 3}); actual != expected {
+	if actual = MaximumRevenue([]int{5, 3}); actual != expected {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
@@ -33,8 +21,14 @@ func TestMaximumRevenue(T *testing.T) {
 		T.Fail()
 	}
 
-	expected = 200
-	if actual = MaximumRevenue([]int{1, 100, 1, 100}); actual != expected {
+	expected = 15
+	if actual = MaximumRevenue([]int{5, 3, 7, 10}); actual != expected {
+		T.Logf("Expected %v got %v", expected, actual)
+		T.Fail()
+	}
+
+	expected = 22
+	if actual = MaximumRevenue([]int{8, 15, 3, 7}); actual != expected {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
@@ -45,14 +39,32 @@ func TestMaximumRevenue(T *testing.T) {
 		T.Fail()
 	}
 
-	expected = 14
-	if actual = MaximumRevenue([]int{8, 7, 6, 5}); actual != expected {
+	expected = 18
+	if actual = MaximumRevenue([]int{10, 9, 8, 7}); actual != expected {
+		T.Logf("Expected %v got %v", expected, actual)
+		T.Fail()
+	}
+
+	expected = 200
+	if actual = MaximumRevenue([]int{100, 1, 100, 1}); actual != expected {
+		T.Logf("Expected %v got %v", expected, actual)
+		T.Fail()
+	}
+
+	expected = 101
+	if actual = MaximumRevenue([]int{100, 1, 1, 100}); actual != expected {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
 
 	expected = 42
 	if actual = MaximumRevenue([]int{20, 30, 2, 2, 2, 10}); actual != expected {
+		T.Logf("Expected %v got %v", expected, actual)
+		T.Fail()
+	}
+
+	expected = 27
+	if actual = MaximumRevenue([]int{6, 9, 1, 2, 16, 8, 3, 4}); actual != expected {
 		T.Logf("Expected %v got %v", expected, actual)
 		T.Fail()
 	}
