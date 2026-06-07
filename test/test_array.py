f705ebfbe7bda7c4beedd3fd27ed6227292efd6a
quang.phan <ptquang2000@gmail.com>
2026-06-07 07:07:52 +0700

    upd array tcs

--- test/test_array.py
+++ test/test_array.py
@@ -23,6 +23,18 @@ class TestArray(unittest.TestCase):
         A = ['a','b','c','d']
         apply_permutation([2,0,1,3], A)
         self.assertEqual(A, ['b','c','a','d'])
+        A = ['a','b','c','d']
+        apply_permutation([0,1,2,3], A)
+        self.assertEqual(A, ['a','b','c','d'])
+        A = ['x']
+        apply_permutation([0], A)
+        self.assertEqual(A, ['x'])
+        A = ['p','q']
+        apply_permutation([1,0], A)
+        self.assertEqual(A, ['q','p'])
+        A = ['a','b']
+        apply_permutation([0,1], A)
+        self.assertEqual(A, ['a','b'])
 
     def test_buy_and_sell_stock_once(self):
         A = [310,315,275,295,260,270,290,230,255,250]
@@ -31,6 +43,11 @@ class TestArray(unittest.TestCase):
         self.assertEqual(buy_and_sell_stock_once([1,2]), 1)
         self.assertEqual(buy_and_sell_stock_once([1,1,1,1]), 0)
         self.assertEqual(buy_and_sell_stock_once([2,1,4]), 3)
+        self.assertEqual(buy_and_sell_stock_once([1]), 0)
+        self.assertEqual(buy_and_sell_stock_once([7,1,5,3,6,4]), 5)
+        self.assertEqual(buy_and_sell_stock_once([7,6,4,3,1]), 0)
+        self.assertEqual(buy_and_sell_stock_once([1,2,3,4,5]), 4)
+        self.assertEqual(buy_and_sell_stock_once([5,4,3,2,1,6]), 5)
 
     def test_buy_and_sell_stock_twice(self):
         A = [12,11,13,9,12,8,14,13,15]
@@ -39,16 +56,47 @@ class TestArray(unittest.TestCase):
         self.assertEqual(buy_and_sell_stock_twice([1,2,3,4,5]), 4)
         self.assertEqual(buy_and_sell_stock_twice([7,6,4,3,1]), 0)
         self.assertEqual(buy_and_sell_stock_twice([1,2,4,2,5,7,2,4,9,0]), 13)
+        self.assertEqual(buy_and_sell_stock_twice([1]), 0)
+        self.assertEqual(buy_and_sell_stock_twice([1,2]), 1)
+        self.assertEqual(buy_and_sell_stock_twice([2,1]), 0)
+        self.assertEqual(buy_and_sell_stock_twice([1,5,2,4]), 6)
+        self.assertEqual(buy_and_sell_stock_twice([1,3,1,3]), 4)
 
     def test_can_reach_end(self):
         self.assertTrue(can_reach_end([2,4,1,1,0,2,3]))
         self.assertTrue(can_reach_end([3,3,7,0,2,0,1]))
         self.assertFalse(can_reach_end([3,2,0,0,2,0,7]))
+        self.assertTrue(can_reach_end([]))
+        self.assertTrue(can_reach_end([0]))
+        self.assertTrue(can_reach_end([1]))
+        self.assertTrue(can_reach_end([1,0]))
+        self.assertFalse(can_reach_end([0,1]))
+        self.assertTrue(can_reach_end([3,0,0,0]))
+        self.assertTrue(can_reach_end([2,0,0]))
+        self.assertTrue(can_reach_end([2,3,0,0]))
+        self.assertFalse(can_reach_end([0,0,0]))
+        self.assertTrue(can_reach_end([5,0,0,0,0,0]))
 
     def test_delete_duplicates(self):
         A = [2,3,5,5,7,11,11,11,13]
         self.assertEqual(delete_duplicates(A), 6)
         self.assertEqual(A[:6], [2,3,5,7,11,13])
+        A = []
+        self.assertEqual(delete_duplicates(A), 0)
+        A = [5]
+        self.assertEqual(delete_duplicates(A), 1)
+        A = [1,2,3,4,5]
+        self.assertEqual(delete_duplicates(A), 5)
+        self.assertEqual(A[:5], [1,2,3,4,5])
+        A = [1,1,1,1,1]
+        self.assertEqual(delete_duplicates(A), 1)
+        self.assertEqual(A[:1], [1])
+        A = [0,0,1,1,2,2,3,3]
+        self.assertEqual(delete_duplicates(A), 4)
+        self.assertEqual(A[:4], [0,1,2,3])
+        A = [1,1,2,2,3,3]
+        self.assertEqual(delete_duplicates(A), 3)
+        self.assertEqual(A[:3], [1,2,3])
 
     def _dutch_inner(self, pivot_idx, A):
         pivot = A[pivot_idx]
@@ -57,9 +105,12 @@ class TestArray(unittest.TestCase):
         A.reverse()
         last = len(A) - 1 - A.index(pivot)
         A.reverse()
-        self.assertEqual([i < pivot for i in A[0:first]], [True for _ in A[0:first]])
-        self.assertEqual([i == pivot for i in A[first:last + 1]], [True for _ in A[first:last + 1]])
-        self.assertEqual([i > pivot for i in A[last + 1:]], [True for _ in A[last + 1:]])
+        self.assertEqual([i < pivot for i in A[0:first]], [True for _ in A[0:first]],
+                         f"Elements before pivot should be < {pivot}, got {A[0:first]}")
+        self.assertEqual([i == pivot for i in A[first:last + 1]], [True for _ in A[first:last + 1]],
+                         f"Elements from {first} to {last} should be == {pivot}, got {A[first:last + 1]}")
+        self.assertEqual([i > pivot for i in A[last + 1:]], [True for _ in A[last + 1:]],
+                         f"Elements after index {last} should be > {pivot}, got {A[last + 1:]}")
 
     def test_dutch_flag_partition(self):
         A = [0,1,2,0,2,1,1]
@@ -71,6 +122,15 @@ class TestArray(unittest.TestCase):
         self._dutch_inner(0, [5][:])
         self._dutch_inner(1, [3,1,4,1,5,9,2,6][:])
         self._dutch_inner(4, [3,1,4,1,5,9,2,6][:])
+        self._dutch_inner(0, [1,0,2][:])
+        self._dutch_inner(1, [1,0,2][:])
+        self._dutch_inner(2, [1,0,2][:])
+        self._dutch_inner(0, [3,3,3,3][:])
+        self._dutch_inner(0, [0,0,0][:])
+        self._dutch_inner(0, [7,3,5,3,7,5][:])
+        self._dutch_inner(1, [2,2,1,1][:])
+        self._dutch_inner(0, [1,2,0,2,0][:])
+        self._dutch_inner(2, [5,4,3,2,1][:])
 
     def test_generate_primes(self):
         self.assertEqual(generate_primes(18), [2,3,5,7,11,13,17])
@@ -78,6 +138,10 @@ class TestArray(unittest.TestCase):
         self.assertEqual(generate_primes(2), [2])
         self.assertEqual(generate_primes(10), [2,3,5,7])
         self.assertEqual(generate_primes(30), [2,3,5,7,11,13,17,19,23,29])
+        self.assertEqual(generate_primes(0), [])
+        self.assertEqual(generate_primes(3), [2,3])
+        self.assertEqual(generate_primes(50), [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47])
+        self.assertEqual(generate_primes(100), [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
 
     def test_multiply(self):
         num1 = [1,9,3,7,0,7,7,2,1]
@@ -87,15 +151,40 @@ class TestArray(unittest.TestCase):
         self.assertEqual(multiply([-1,2], [3]), [-3,6])
         self.assertEqual(multiply([0], [9,9,9]), [0])
         self.assertEqual(multiply([9,9], [9,9]), [9,8,0,1])
+        self.assertEqual(multiply([0], [0]), [0])
+        self.assertEqual(multiply([1], [1]), [1])
+        self.assertEqual(multiply([1], [9]), [9])
+        self.assertEqual(multiply([9], [1]), [9])
+        self.assertEqual(multiply([2,3], [4]), [9,2])
+        self.assertEqual(multiply([5], [5]), [2,5])
+        self.assertEqual(multiply([-1,2], [-3]), [3,6])
+        self.assertEqual(multiply([-9], [9]), [-8,1])
+        self.assertEqual(multiply([1,0,0], [1,0]), [1,0,0,0])
 
     def test_next_permutation(self):
         self.assertEqual(next_permutation([1,0,3,2]), [1,2,0,3])
         self.assertEqual(next_permutation([3,2,1,0]), [])
         self.assertEqual(next_permutation([6,2,1,5,4,3,0]), [6,2,3,0,1,4,5])
+        self.assertEqual(next_permutation([1,2]), [2,1])
+        self.assertEqual(next_permutation([2,1]), [])
+        self.assertEqual(next_permutation([1,1,1]), [])
+        self.assertEqual(next_permutation([1]), [])
+        self.assertEqual(next_permutation([1,3,2]), [2,1,3])
+        self.assertEqual(next_permutation([1,2,3]), [1,3,2])
+        self.assertEqual(next_permutation([1,1,5,1]), [1,5,1,1])
+        self.assertEqual(next_permutation([2,3,1]), [3,1,2])
+        self.assertEqual(next_permutation([1,2,3,4]), [1,2,4,3])
 
     def test_plus_one(self):
         self.assertEqual(plus_one([1,2,9]), [1,3,0])
         self.assertEqual(plus_one([9,9,9]), [1,0,0,0])
+        self.assertEqual(plus_one([0]), [1])
+        self.assertEqual(plus_one([9]), [1,0])
+        self.assertEqual(plus_one([1,9,9]), [2,0,0])
+        self.assertEqual(plus_one([8,9,9,9]), [9,0,0,0])
+        self.assertEqual(plus_one([1,0,0]), [1,0,1])
+        self.assertEqual(plus_one([9,0,9]), [9,1,0])
+        self.assertEqual(plus_one([1,2,3]), [1,2,4])
 
     def test_merge(self):
         nums1 = [1, 2, 3, 0, 0, 0]
@@ -109,6 +198,15 @@ class TestArray(unittest.TestCase):
         nums1 = [0]
         merge(nums1, 0, [1], 1)
         self.assertEqual(nums1, [1])
+        nums1 = [4, 5, 6, 0, 0, 0]
+        merge(nums1, 3, [1, 2, 3], 3)
+        self.assertEqual(nums1, [1, 2, 3, 4, 5, 6])
+        nums1 = [0, 0, 0]
+        merge(nums1, 0, [1, 2, 3], 3)
+        self.assertEqual(nums1, [1, 2, 3])
+        nums1 = [1, 2, 3]
+        merge(nums1, 3, [], 0)
+        self.assertEqual(nums1, [1, 2, 3])
 
     def test_remove_element(self):
         nums = [3, 2, 2, 3]
@@ -120,3 +218,25 @@ class TestArray(unittest.TestCase):
         k = remove_element(nums, 2)
         self.assertEqual(k, 5)
         self.assertEqual(sorted(nums[:k]), [0, 0, 1, 3, 4])
+        nums = []
+        k = remove_element(nums, 0)
+        self.assertEqual(k, 0)
+        nums = [1]
+        k = remove_element(nums, 1)
+        self.assertEqual(k, 0)
+        self.assertEqual(nums[:k], [])
+        nums = [1]
+        k = remove_element(nums, 2)
+        self.assertEqual(k, 1)
+        self.assertEqual(nums[:k], [1])
+        nums = [2, 2, 2, 2]
+        k = remove_element(nums, 2)
+        self.assertEqual(k, 0)
+        nums = [1, 2, 3, 4, 5]
+        k = remove_element(nums, 3)
+        self.assertEqual(k, 4)
+        self.assertEqual(sorted(nums[:k]), [1, 2, 4, 5])
+        nums = [0, 1, 0, 3, 0]
+        k = remove_element(nums, 0)
+        self.assertEqual(k, 2)
+        self.assertEqual(sorted(nums[:k]), [1, 3])
--- test/test_multi_dim_array.py
+++ test/test_multi_dim_array.py
@@ -21,6 +21,18 @@ class TestMultiDimArray(unittest.TestCase):
         self.assertEqual(generate_pascal_triangle(1), [[1]])
         self.assertEqual(generate_pascal_triangle(2), [[1],[1,1]])
         self.assertEqual(generate_pascal_triangle(3), [[1],[1,1],[1,2,1]])
+        self.assertEqual(generate_pascal_triangle(4), [[1],[1,1],[1,2,1],[1,3,3,1]])
+        self.assertEqual(generate_pascal_triangle(6), [
+            [1],
+            [1,1],
+            [1,2,1],
+            [1,3,3,1],
+            [1,4,6,4,1],
+            [1,5,10,10,5,1],
+        ])
+        triangle = generate_pascal_triangle(7)
+        self.assertEqual(triangle[6], [1,6,15,20,15,6,1])
+        self.assertEqual(triangle[5], [1,5,10,10,5,1])
 
     def test_is_valid_sudoku(self):
         partial_sudoku = [
@@ -59,6 +71,30 @@ class TestMultiDimArray(unittest.TestCase):
             [3,4,5,2,8,6,1,7,9],
         ]
         self.assertFalse(is_valid_sudoku(completed_sudoku))
+        col_duplicate = [
+            [1,0,0,0,0,0,0,0,0],
+            [2,0,0,0,0,0,0,0,0],
+            [3,0,0,0,0,0,0,0,0],
+            [4,0,0,0,0,0,0,0,0],
+            [5,0,0,0,0,0,0,0,0],
+            [6,0,0,0,0,0,0,0,0],
+            [7,0,0,0,0,0,0,0,0],
+            [8,0,0,0,0,0,0,0,0],
+            [1,0,0,0,0,0,0,0,0],
+        ]
+        self.assertFalse(is_valid_sudoku(col_duplicate))
+        another_valid = [
+            [1,2,3,4,5,6,7,8,9],
+            [4,5,6,7,8,9,1,2,3],
+            [7,8,9,1,2,3,4,5,6],
+            [2,3,1,5,6,4,8,9,7],
+            [5,6,4,8,9,7,2,3,1],
+            [8,9,7,2,3,1,5,6,4],
+            [3,1,2,6,4,5,9,7,8],
+            [6,4,5,9,7,8,3,1,2],
+            [9,7,8,3,1,2,6,4,5],
+        ]
+        self.assertTrue(is_valid_sudoku(another_valid))
 
     def test_matrix_in_spiral_order(self):
         matrix = [
@@ -75,9 +111,14 @@ class TestMultiDimArray(unittest.TestCase):
         ]
         self.assertEqual(matrix_in_spiral_order(matrix), [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10])
         self.assertEqual(matrix_in_spiral_order([[7]]), [7])
-        self.assertEqual(matrix_in_spiral_order([[1,2,3]]), [1,2,3])
-        self.assertEqual(matrix_in_spiral_order([[1],[2],[3]]), [1,2,3])
-        self.assertEqual(matrix_in_spiral_order([[1,2],[3,4]]), [1,2,4,3])
+        matrix5 = [
+            [1,2,3,4,5],
+            [6,7,8,9,10],
+            [11,12,13,14,15],
+            [16,17,18,19,20],
+            [21,22,23,24,25],
+        ]
+        self.assertEqual(matrix_in_spiral_order(matrix5), [1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6,7,8,9,14,19,18,17,12,13])
 
     def test_rotate_matrix(self):
         matrix = [
@@ -110,3 +151,19 @@ class TestMultiDimArray(unittest.TestCase):
         ]
         rotate_matrix(matrix)
         self.assertEqual(matrix, [[7,4,1],[8,5,2],[9,6,3]])
+        matrix = [
+            [1,2,3,4,5],
+            [6,7,8,9,10],
+            [11,12,13,14,15],
+            [16,17,18,19,20],
+            [21,22,23,24,25],
+        ]
+        rotated_matrix = [
+            [21,16,11,6,1],
+            [22,17,12,7,2],
+            [23,18,13,8,3],
+            [24,19,14,9,4],
+            [25,20,15,10,5],
+        ]
+        rotate_matrix(matrix)
+        self.assertEqual(matrix, rotated_matrix)
